803d5ff0b759d49432e0a8ea19485f44820e3cb7
Chase <chaseaohill@gmail.com>
2023-03-06 01:26:12 -0500

    Completed first custom cell and fixed improper label name.

--- Assessment 5 iTunes/Assessment 5 iTunes/Controllers/Custom Cells/AlbumTableViewCell.swift
+++ Assessment 5 iTunes/Assessment 5 iTunes/Controllers/Custom Cells/AlbumTableViewCell.swift
@@ -12,8 +12,23 @@ class AlbumTableViewCell: UITableViewCell {
     // MARK: - Outlets
     
     @IBOutlet weak var albumCoverImageView: UIImageView!
-    @IBOutlet weak var albumName: UILabel!
+    @IBOutlet weak var albumNameLabel: UILabel!
     @IBOutlet weak var songsInAlbumLabel: UILabel!
     
-    
+    // MARK: - Functions
+    func fetchAlbums(for album: Album) {
+        
+        albumNameLabel.text = album.albumName
+        songsInAlbumLabel.text = "\(album.songCount) Songs"
+        AlbumService.fetchAlbumCover(forAlbum: album) { [weak self] result in
+            switch result {
+            case .success(let cover):
+                DispatchQueue.main.async {
+                    self?.albumCoverImageView.image = cover
+                }
+            case .failure(let error):
+                print(error.errorDescription ?? NetworkError.unknownError)
+            }
+        }
+    }
 }
