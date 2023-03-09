26ae512f163ecab88ec39dce7673bf060dfb2f74
Chase <chaseaohill@gmail.com>
2023-03-08 23:46:34 -0500

    Completed Song model

--- Assessment 5 iTunes/Assessment 5 iTunes/Models/Song.swift
+++ Assessment 5 iTunes/Assessment 5 iTunes/Models/Song.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct SongTopLevelDictionary: Decodable {
+    
+    let results: [Song]
+}
+
+struct Song: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case songName           = "trackName"
+        case songNumber         = "trackNumber"
+        case songLength         = "trackTimeMillis"
+        case songArtistName     = "artistName"
+        case songFromAlbumName  = "collectionName"
+    }
+    
+    let songName: String
+    let songNumber: Int
+    let songLength: Int
+    let songArtistName: String
+    let songFromAlbumName: String
+}
